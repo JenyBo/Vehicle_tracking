95f389c947315934b27718e9a7579cc45e8cd1eb
jenybo <86452879+JenyBo@users.noreply.github.com>
2024-05-03 16:33:27 +0700

    update_reason

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
+import 'package:geolocator/geolocator.dart';
+import 'package:camera/camera.dart';
 import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart' as l;
 import 'package:permission_handler/permission_handler.dart';
+import 'camera_screen.dart';
 import 'location_service.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,11 +17,13 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final LocationService locationService = LocationService();
+  late List<CameraDescription> cameras;
+
   bool gpsEnabled = false;
   bool permissionGranted = false;
   late StreamSubscription<l.LocationData> subscription;
   bool trackingEnabled = false;
-
+  List<l.LocationData> lastThreeLocations = [];
   List<Map<String, dynamic>> locations = [];
   int countdown = 5;
   bool isCountdownRunning = false;
@@ -28,6 +33,7 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     initLocationService();
+    initCameras();
   }
 
   @override
@@ -36,30 +42,21 @@ class _HomeScreenState extends State<HomeScreen> {
     super.dispose();
   }
 
+  void initCameras() async {
+    PermissionStatus status = await Permission.camera.request();
+    if (status.isGranted) {
+      cameras = await availableCameras();
+    } else {
+      // You can show a message to the user indicating that the camera permission was not granted.
+      print('Camera permission was not granted');
+    }
+  }
+
   void initLocationService() async {
     await locationService.checkStatus();
     setState(() {
       gpsEnabled = locationService.gpsEnabled;
       permissionGranted = locationService.permissionGranted;
-    });
-  }
-
-  void startCountDown() {
-    if (isCountdownRunning) {
-      return;
-    }
-    isCountdownRunning = true;
-    Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {
-        countdown--;
-      });
-      if (countdown == 0) {
-        timer.cancel();
-        isCountdownRunning = false;
-        setState(() {
-          countdown = 5;
-        });
-      }
     });
   }
 
@@ -71,18 +68,117 @@ class _HomeScreenState extends State<HomeScreen> {
       addLocation(event);
       startCountDown();
     });
-    timer = Timer.periodic(const Duration(seconds: 5), (timer) async {
-      l.LocationData currentLocation = await locationService.location.getLocation();
-      addLocation(currentLocation);
-    });
     setState(() {
       trackingEnabled = true;
     });
   }
 
+  void startCountDown() {
+    if (isCountdownRunning) {
+      return;
+    }
+    countdown = 5;
+    isCountdownRunning = true;
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) async {
+      if (countdown == 1) {
+        timer.cancel();
+        isCountdownRunning = false;
+        l.LocationData? currentLocation;
+        try {
+          currentLocation = await locationService.location.getLocation();
+        } catch (e) {
+          print('Failed to get location: $e');
+        }
+        if (currentLocation != null) {
+          addLocation(currentLocation);
+          if (areLastThreeLocationsSame(lastThreeLocations, 10.0)) { // 10.0 is the threshold in meters
+            showDialog(
+              context: context,
+              builder: (context) {
+                String dropdownValue = 'Road blockage';
+                String otherReason = '';
+                return StatefulBuilder(
+                  builder: (context, setState) {
+                    return AlertDialog(
+                      title: const Text('You are stopped !!!'),
+                      content: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          DropdownButton<String>(
+                            value: dropdownValue,
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                dropdownValue = newValue!;
+                              });
+                            },
+                            items: <String>['Road blockage', 'Red light', 'Other reason']
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                          ),
+                          if (dropdownValue == 'Other reason')
+                            TextField(
+                              onChanged: (value) {
+                                otherReason = value;
+                              },
+                              decoration: const InputDecoration(
+                                hintText: 'Enter your reason',
+                              ),
+                            ),
+                        ],
+                      ),
+                      actions: <Widget>[
+                        TextButton(
+                          child: Text('OK'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                            print('Selected reason: $dropdownValue');
+                            if (dropdownValue == 'Other reason') {
+                              print('Other reason: $otherReason');
+                            }
+                          },
+                        ),
+                        if (dropdownValue == 'traffic jam' || dropdownValue == 'red light')
+                          TextButton(
+                            child: Text('Take Picture'),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => TakePictureScreen(
+                                    camera: cameras[0], // Pass the appropriate camera to the TakePictureScreen widget.
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                      ],
+                    );
+                  },
+                );
+              },
+            );
+            stopTracking();
+            return;
+          }
+        }
+        startCountDown();
+      } else {
+        setState(() {
+          countdown--;
+        });
+      }
+    });
+  }
+
   void stopTracking() {
+    if (timer.isActive) {
+      timer.cancel();
+    }
     subscription.cancel();
-    timer.cancel();
     setState(() {
       trackingEnabled = false;
     });
@@ -97,6 +193,11 @@ class _HomeScreenState extends State<HomeScreen> {
       } else {
         locations.insert(0, {'location': data, 'time': updateTime});
       }
+
+      lastThreeLocations.insert(0, data);
+      if (lastThreeLocations.length > 3) {
+        lastThreeLocations.removeLast();
+      }
     });
   }
 
@@ -104,6 +205,28 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       locations.clear();
     });
+  }
+
+  bool areLocationsClose(l.LocationData loc1, l.LocationData loc2, double thresholdInMeters) {
+    double distanceInMeters = Geolocator.distanceBetween(
+      loc1.latitude!,
+      loc1.longitude!,
+      loc2.latitude!,
+      loc2.longitude!,
+    );
+
+    return distanceInMeters <= thresholdInMeters;
+  }
+
+  bool areLastThreeLocationsSame(List<l.LocationData> lastThreeLocations, double thresholdInMeters) {
+    if (lastThreeLocations.length < 3) {
+      return false;
+    }
+
+    bool isSameLocation1And2 = areLocationsClose(lastThreeLocations[0], lastThreeLocations[1], thresholdInMeters);
+    bool isSameLocation2And3 = areLocationsClose(lastThreeLocations[1], lastThreeLocations[2], thresholdInMeters);
+
+    return isSameLocation1And2 && isSameLocation2And3;
   }
 
   @override
